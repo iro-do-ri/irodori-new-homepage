1cfaba04c92b0c280822e9e102886226a211a9fc
iro-do-ri <k-katsuno@iro-do-ri.jp>
2026-03-02 18:28:48 +0900

    add console error

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -60,6 +60,7 @@ export async function POST(req: Request) {
     return NextResponse.json({ success: true });
 
   } catch (error) {
+    console.error("MAIL ERROR:", error);
     return NextResponse.json({ success: false }, { status: 500 });
   }
   

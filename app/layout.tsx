0ce0a94b13eb87da93a6a0deb59bea4400ed6c6f
iro-do-ri <k-katsuno@iro-do-ri.jp>
2026-03-09 14:36:31 +0900

    modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Murecho } from "next/font/google";
 import "./globals.css";
 import Footer from "./parts/Footer";
+import Script from "next/script";
 
 const MontserratSans = Montserrat({
   variable: "--font-Montserrat",
@@ -78,21 +79,29 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
-      <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-T9ZN2XDC');
-            `,
-          }}
-        />
+      <Script
+        id="gtm"
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T9ZN2XDC');
+          `,
+        }}
+      />
       </head>
       <body>
       <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9ZN2XDC" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-T9ZN2XDC"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        />
+      </noscript>
         {children}
         <Footer />
       </body>

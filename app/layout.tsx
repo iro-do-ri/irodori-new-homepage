bb489400dcf8354fb0f4df674b909cc0c8d5c633
iro-do-ri <k-katsuno@iro-do-ri.jp>
2026-03-09 14:24:34 +0900

    modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -78,19 +78,23 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+      <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T9ZN2XDC');
+            `,
+          }}
+        />
       </head>
       <body>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T9ZN2XDC" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         {children}
         <Footer />
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-19CK6TNK8W"></script>
-      <script>
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-19CK6TNK8W');
-        `}
-      </script>
       </body>
     </html>
   );

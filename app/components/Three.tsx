ea597efa39ebb0c857ef03f1ec369daa897dd65e
iro-do-ri <k-katsuno@iro-do-ri.jp>
2026-03-16 12:43:51 +0900

    modify

--- app/components/Three.tsx
+++ app/components/Three.tsx
@@ -17,7 +17,7 @@ export default function Three() {
         <ambientLight intensity={2} />
         <directionalLight
           castShadow
-          position={isMobile ? [10, 4, 2] : [2, 4, 2]}
+          position={isMobile ? [30, 4, 2] : [2, 4, 2]}
           shadow-mapSize-width={1024}
           shadow-mapSize-height={1024}
         />

2fce6bc23363595353de5b801aad8799b6d6befd
iro-do-ri <k-katsuno@iro-do-ri.jp>
2026-04-18 18:25:27 +0900

    modify

--- app/components/Three.tsx
+++ app/components/Three.tsx
@@ -13,7 +13,7 @@ export default function Three() {
   }, []);
 
   return (
-    <div style={{ width: "100%", height: isMobile ? "105svh" : "100dvh", position: "fixed", zIndex: "-1", background: "adial-gradient(circle at 17% 77%, rgba(17, 17, 17,0.04) 0%, rgba(17, 17, 17,0.04) 50%,rgba(197, 197, 197,0.04) 50%, rgba(197, 197, 197,0.04) 100%),radial-gradient(circle at 26% 17%, rgba(64, 64, 64,0.04) 0%, rgba(64, 64, 64,0.04) 50%,rgba(244, 244, 244,0.04) 50%, rgba(244, 244, 244,0.04) 100%),radial-gradient(circle at 44% 60%, rgba(177, 177, 177,0.04) 0%, rgba(177, 177, 177,0.04) 50%,rgba(187, 187, 187,0.04) 50%, rgba(187, 187, 187,0.04) 100%),linear-gradient(19deg, rgb(28, 117, 250),rgb(34, 2, 159))" }}>
+    <div style={{ width: "100%", height: isMobile ? "105svh" : "100dvh", position: "fixed", zIndex: "-1", background: "radial-gradient(circle at 17% 77%, rgba(17, 17, 17,0.04) 0%, rgba(17, 17, 17,0.04) 50%,rgba(197, 197, 197,0.04) 50%, rgba(197, 197, 197,0.04) 100%),radial-gradient(circle at 26% 17%, rgba(64, 64, 64,0.04) 0%, rgba(64, 64, 64,0.04) 50%,rgba(244, 244, 244,0.04) 50%, rgba(244, 244, 244,0.04) 100%),radial-gradient(circle at 44% 60%, rgba(177, 177, 177,0.04) 0%, rgba(177, 177, 177,0.04) 50%,rgba(187, 187, 187,0.04) 50%, rgba(187, 187, 187,0.04) 100%),linear-gradient(19deg, rgb(28, 117, 250),rgb(34, 2, 159))" }}>
       <Canvas shadows camera={{ position: [39.6, 15.4, 44.5], fov: 10 }}>
         <ambientLight intensity={1.2} />
         <directionalLight
